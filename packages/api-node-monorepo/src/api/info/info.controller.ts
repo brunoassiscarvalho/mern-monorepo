d69f1d520f42242af85f6ba37736bb42cb59111e
Bruno Assis Carvalho <brunoacarvalho@live.com>
2023-03-19 01:49:46 -0300

    exclude old api

--- packages/api-node-monorepo/src/api/info/info.controller.ts
+++
@@ -1,6 +0,0 @@
-// const Contact = mongoose.model("Contact", ContactSchema);
-export default class InfoController {
-  public async getInfos(): Promise<string> {
-    return "Servidor Funcionado!";
-  }
-}
