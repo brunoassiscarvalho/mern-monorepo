edeccc40692e7bae2c11b159415a4d1c139b5dc3
Bruno A Carvalho <{ID}+{username}@users.noreply.github.com>
2025-07-14 05:19:57 -0300

    without business interfaces

--- packages/interfaces/index.ts
+++ packages/interfaces/index.ts
@@ -1,6 +1,2 @@
 export type { IInfo } from "./src/Info";
-export type { IPatient, IPatientBasic } from "./src/Patient";
-export type { IProfessional } from "./src/Professional";
-export type { IAppointment } from "./src/Appointment";
-export type { IPatientSymptom } from "./src/PatientSymptom";
-export type { Symptom } from "./src/Symptom";
+
--- packages/interfaces/src/Professional.ts
+++
@@ -1,7 +0,0 @@
-import { Positions } from '../utils/TypeEnums';
-
-export interface IProfessional {
-  _id?: string;
-  name: string;
-  position: Positions;
-}
